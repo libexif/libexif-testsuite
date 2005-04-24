8e71fc378238c2647537aa4f7722ef6eef0ecede
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-04-24 10:59:24 +0200

    testlib.c: compile cleanly

--- testlib/testlib.c
+++ testlib/testlib.c
@@ -2,10 +2,10 @@
 
 #define N_(string) string
 
+const char *testlib_test(void);
+
 static const char *testlib_string = N_("This is the dummy library");
 
-const char *testlib_test();
-
-const char *testlib_test() {
+const char *testlib_test(void) {
   return testlib_string;
 }
