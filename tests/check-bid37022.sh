a4f3026364d537d3b6c69888a758b981b9eecee3
Dan Fandrich <dan@coneharvesters.com>
2009-11-16 14:49:31 -0800

    Added check-bid37022.sh to test for the heap overflow problem in libexif. Released 0.7.15

---
+++ tests/check-bid37022.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Checks that libexif doesn't crash when reading a invalid image with a BYTE
+# array that is fixed up to SHORT. libexif 0.6.18 overflowed the heap in
+# that case.  This isn't a complete test because a heap overflow won't
+# always cause a crash, but a malloc library which checks heap consistency
+# should catch it (such as glibc does when linked with -lmcheck).
+. check-vars.sh
+bug="bid37022"
+srcimg="$SRCDIR/${bug}.jpg"
+
+echo "Reading EXIF image"
+env LANG=C LANGUAGE=C "$EXIFEXE" "$srcimg" >/dev/null
+
+# if the program doesn't crash it's a successful test
