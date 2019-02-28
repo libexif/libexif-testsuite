df7ca1092a381e8633d7e59ae31c5f2e2d0e7124
Dan Fandrich <dan@coneharvesters.com>
2019-03-01 00:28:55 +0100

    Added test of nonwritable JPEG file

--- tests/check-write-fail.sh
+++ tests/check-write-fail.sh
@@ -13,9 +13,14 @@ failed="0"
 
 touch "$tmpfile"
 chmod 444 "$tmpfile"
-$EXIFEXE --create-exif --extract-thumbnail -o "$tmpfile" "$SRCDIR/images/canon-powershot-g2-001.jpg"
-test $? -eq 1 || exit 1
-s="$?"
 
+echo Check writing JPEG to unwritable file fails cleanly
+$EXIFEXE -r -o "$tmpfile" "$SRCDIR/images/canon-powershot-g2-001.jpg" >/dev/null
+test $? -eq 1 || exit 1
+
+echo Check writing thumbnail to unwritable file fails cleanly
+$EXIFEXE --extract-thumbnail -o "$tmpfile" "$SRCDIR/images/canon-powershot-g2-001.jpg"
+test $? -eq 1 || exit 1
+
+echo PASSED
 rm -f "$tmpfile"
-exit "$s"
